51da6e727fccc060f1670645197971e1198969c6
enzojpn <enzo_jpn@hotmail.com>
2023-10-23 23:40:05 -0300

    adicionando cors

--- src/main/java/com/example/primefacesdemo/PrimefacesDemoApplication.java
+++ src/main/java/com/example/primefacesdemo/PrimefacesDemoApplication.java
@@ -1,14 +1,19 @@
 package com.example.primefacesdemo;
 
+import java.util.Arrays;
+
+import javax.faces.webapp.FacesServlet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import javax.faces.webapp.FacesServlet;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @EnableAutoConfiguration
 @ComponentScan({"com.example.primefacesdemo"})
@@ -18,6 +23,7 @@ public class PrimefacesDemoApplication extends SpringBootServletInitializer {
     public static void main(String[] args) {
         SpringApplication.run(PrimefacesDemoApplication.class, args);
     }
+ 
 
     @Bean
     public ServletRegistrationBean<?> facesServletRegistration() {
